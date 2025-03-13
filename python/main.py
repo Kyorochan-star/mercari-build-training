62b812d418c850d941bb891259286f818b8722d3
Yuka Kanasaki <341117lisamilet@gmail.com>
2025-03-13 16:02:30 +0900

    step6_first

--- python/main.py
+++ python/main.py
@@ -4,11 +4,7 @@ import pathlib
 import json
 import hashlib
 from pathlib import Path
-#<<<<<<< feature/step4
-#from fastapi import FastAPI, Form, HTTPException, File, UploadFile, Depends
-#=======
 from fastapi import FastAPI, Form, HTTPException, File, UploadFile, Depends, Query
-#>>>>>>> main
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
@@ -124,36 +120,7 @@ async def add_item(
     image: UploadFile = File(...),
     db: sqlite3.Connection = Depends(get_db),
 ):
-#<<<<<<< feature/step4
 
-    # 画像をSHA-256でハッシュ化して保存
-    #image_bytes = await image.read()  # 画像データを読み込む
-    #sha256_hash = hashlib.sha256(image_bytes).hexdigest()  # SHA-256ハッシュ化
-    #image_filename = f"{sha256_hash}.jpg"  # ハッシュ値をファイル名にして.jpg拡張子を追加
-
-
-    # 画像を保存するディレクトリを確認、なければ作成
-    #image_dir = Path("images")
-    #if not image_dir.exists():
-        #image_dir.mkdir(parents=True)
-
-    # 画像保存パス
-    #image_path = image_dir / image_filename
-
-    # 画像ファイルを保存
-    #with open(image_path, "wb") as f:
-        #f.write(image_bytes)
-
-    # 入力チェック
-    #if not name or not category:
-        #raise HTTPException(status_code=400, detail="Both name and category are required")
-
-    # アイテムの作成
-    #item = Item(name=name, category=category, image_name=image_filename)
-    #insert_item(item)  # アイテムを保存
-
-    #return AddItemResponse(**{"message": f"item received: name: {name}, category: {category}, image name: {item.image_name}"})
-#=======
     # 入力チェック
     if not name or not category:
         raise HTTPException(status_code=400, detail="Both name and category are required")
@@ -201,7 +168,6 @@ async def add_item(
     item_id = cur.lastrowid  #  追加したアイテムの `id` を取得
 
     return AddItemResponse(message="Item added successfully", id=cur.lastrowid)
-#>>>>>>> main
 
 
 
@@ -236,33 +202,7 @@ async def get_image(image_name):
     return FileResponse(image)
 
 
-#<<<<<<< feature/step4
-#def get_items_from_file():
-    #ITEMS_FILE = "items.json"
-    
-    # JSONファイルを読み込む
-   # try:
-        #with open(ITEMS_FILE, "r") as f:
-            #data = json.load(f)
-    #except FileNotFoundError:
-        #raise HTTPException(status_code=404, detail="No items found")
-    
-    #return data["items"]
 
-
-# 商品詳細情報を取得するエンドポイント
-#@app.get("/items/{item_id}")
-#async def get_item(item_id: int):
-    #items = get_items_from_file()  # 商品一覧を取得
-
-    # item_idが範囲外の場合
-    #if item_id < 1 or item_id > len(items):
-        #raise HTTPException(status_code=404, detail="Item not found")
-
-    # item_id番目の商品情報を返す
-    #item = items[item_id - 1]  # インデックスは0から始まるので1を引いて取得
-    #return item
-#=======
 class ItemsResponse(BaseModel):
     message: str
     items: list[dict]
@@ -280,37 +220,11 @@ async def get_items(db: sqlite3.Connection = Depends(get_db)):
 
     return ItemsResponse(message="Items fetched successfully", items=items)
 
-#>>>>>>> main
 
 class Item(BaseModel):
     id: int
     name: str
     category: str
-#<<<<<<< feature/step4
-    #image_name: str = None # 画像のファイル名（オプション）
-
-
-#def insert_item(item: Item):
-    # STEP 4-2: add an implementation to store an item
-    #ITEMS_FILE="items.json"
-
-    # JSONファイルを読み込む
-    #try:
-        #with open(ITEMS_FILE, "r") as f:
-            #data = json.load(f)
-    #except FileNotFoundError:
-        # ファイルがない場合は新しく作成
-        #data = {"items": []}
-    
-    # 新しいアイテムを追加
-    #new_item = {"name": item.name, "category": item.category, "image_name": item.image_name}
-    #data["items"].append(new_item)
-    
-    # ファイルに保存
-    #with open(ITEMS_FILE, "w") as f:
-        #json.dump(data, f, indent=4)
-
-#=======
     image_name: str
 
 @app.get("/items/{item_id}", response_model=Item)
@@ -349,4 +263,4 @@ def search_items(keyword: str = Query(..., min_length=1), db: sqlite3.Connection
     return SearchResponse(items=items)
 
 
-#>>>>>>> main
+
